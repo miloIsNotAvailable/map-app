a55cb8f92611a7afdf1062fefbe05f5ac624101b
unknown <milosz20031207@gmail.com>
2022-08-29 13:46:15 +0200

    fix: vercel not finding index.html file

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,5 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@Components": path.resolve( __dirname, 'components/assets' ),
-      "@styles": path.resolve( __dirname, 'styles' )
-    }
-  },
-  build: {
-    minify: false,
-    ssr: true
-  },
   plugins: [react()],
 })
