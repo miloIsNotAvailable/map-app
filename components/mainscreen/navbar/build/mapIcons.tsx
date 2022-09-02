104bd1bdb428ab6d0c23eae16c57c4c020a00d61
unknown <milosz20031207@gmail.com>
2022-09-02 19:28:21 +0200

    fix: icons not showing in production

--- components/mainscreen/navbar/build/mapIcons.tsx
+++ components/mainscreen/navbar/build/mapIcons.tsx
@@ -1,19 +1,25 @@
 import { FC } from "react";
 import { Icon } from "../icons";
 import { styles } from "./NavbarStyles";
+import { default as Home } from '../../../../graphics/icons/home.svg' 
+import { default as Settings } from '../../../../graphics/icons/settings.svg' 
+import { default as CreateCommunity } from '../../../../graphics/icons/create_community.svg' 
 
 const MapIcons: FC = () => {
 
-    const icons = [ "home", "settings", "create_community" ]
+    const icons = [ 
+        { name: Home, title: "home" }, 
+        { name: Settings, title: "settings" }, 
+        { name: CreateCommunity, title: "create community" } 
+    ]
 
     return (
         <div className={ styles.map_icons }>
             {
                 icons.map( ( icon ) => (
                     <Icon 
-                        name={ `../../../../assets/icons/${ icon }.svg` }
-                        title={ icon }
-                        key={ icon }
+                        { ...icon }
+                        key={ icon?.title }
                     />
                 ) )
             }
